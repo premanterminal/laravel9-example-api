9e28c1a2eed453d85ba3ef2b64afa2653fc6e37a
desktop-simp4d <desktop-simp4d@dispenda.com>
2022-02-17 15:00:18 +0700

    add routes api post

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//posts
+Route::apiResource('/posts', App\Http\Controllers\Api\PostController::class);
